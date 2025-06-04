647b6e65f93935b581d34bd7f85efd4ea14d622c
bpmallen <bpmallen@gmx.com>
2025-06-04 09:58:05 +0100

    8.2 - added RegisterPage template

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar";
 import ArtworkListPage from "./pages/ArtworkListPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import { useAuth } from "./contexts/useAuth";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ArtworkListPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
